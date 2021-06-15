17a50437a0543b1c7dffc5e5a3b85585122172ce
Sam Remy <samuel.a.remy@gmail.com>
2021-06-15 18:22:17 -0500

    updated code

--- Code/NewCoinCrowdsale.sol
+++ Code/NewCoinCrowdsale.sol
@@ -7,20 +7,30 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
+
+// Smartcontract that manages the entire CrodSale process, minting NEW (NewCoin) tokens and allowing users to exchange ETH and for NEW.
+
+
 // Inherit the crowdsale contracts
-contract NewCoinSale is Crowdsale, MintedCrowdsale {
+contract NewCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale {
 
     constructor(
-        // Fill in the constructor parameters!
-        uint rate, 
-        address payable wallet, 
-        NewCoin token
+        uint rate, // contract rate in TKNbits >>> 1000000000000000000 TKNbits is equal to 1 TKN
+        address payable wallet, // fundraising wallet. All ETH raised in Crowdsale will accrue at this wallet address
+        NewCoin token, // define token to be used inside  NewCoinSale
+        uint goal, // target number (minumum) tokens to sell. if sales < goal, contract returns ETH to participants
+        uint cap, // the maximum number of tokens to be minted
+        
     )
         // Pass the constructor parameters to the crowdsale contracts.
-        NewCoinSale(rate,wallet,token)
+        NewCoinSale (rate,wallet,token)
+        CappedCrowdsale (goal)
+        TimedCrowdsale (fakenow, fakenow + 1 minutes) // test
+        // TimedCrowdsale (now, now + 24 weeks) // production
+        RefundablePostDeliveryCrowdsale (goal)
         public
     {
-        // constructor can stay empty
+        // constructor can stay empty as logic included in contracts inherited from OpenZeppelin
     }
 }
 
@@ -30,20 +40,22 @@ contract NewCoinSaleDeployer {
     address public token_address;
 
     constructor(
-        // Fill in the constructor parameters!
         string memory name,
         string memory symbol,
-        address payable wallet // this address will receive all Ether raised by the sale
+        address payable wallet, // this address will receive all Ether raised by the sale
+        uint goal
+        //uint fakenow // test
     )
         public
     {
         // Create the NewCoin and keep its address handy
-        NewCoin token = new NewCoin(name, symbol,0);
-        token_address = address(token);
+        NewCoin token = new NewCoin(name, symbol,0); // create new token w params name, symbol, initial supply
+        token_address = address(token); // fetchs token address
         
-        // Create the PewCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
-        NewCoinSale newcoin_sale = new NewCoinSale(1, wallet, token);
-        token_sale_address_address = address(newcoin_sale);
+        // Create the NewCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
+        NewCoinSale token_sale = new NewCoinSale(1, wallet, token, goal, cap, fakenow, fakenow + 1 minutes); // test
+        // NewCoinSale token_sale = new NewCoinSale(1, wallet, token, goal, cap, now, now + 24 weeks); // production
+        token_sale_address_address = address(token_sale);
         
         // make the NewCoinSale contract a minter, then have the NewCoinSaleDeployer renounce its minter role
         token.addMinter(token_sale_address);
