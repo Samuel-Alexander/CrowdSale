2b03781791ad1a026c7edfde0064863bb4f2e41f
Sam Remy <samuel.a.remy@gmail.com>
2021-06-18 08:06:03 -0500

    Update NewCoinCrowdsale.sol

--- Code/NewCoinCrowdsale.sol
+++ Code/NewCoinCrowdsale.sol
@@ -13,21 +13,22 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 
 // Inherit the crowdsale contracts
 contract NewCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale {
-
+    
     constructor(
         uint rate, // contract rate in TKNbits >>> 1000000000000000000 TKNbits is equal to 1 TKN
         address payable wallet, // fundraising wallet. All ETH raised in Crowdsale will accrue at this wallet address
         NewCoin token, // define token to be used inside  NewCoinSale
-        uint goal, // target number (minumum) tokens to sell. if sales < goal, contract returns ETH to participants
-        uint cap, // the maximum number of tokens to be minted
-        
+        uint goal, // target number (minumum) tokens to sell. if sales < goal, contract returns ETH to participants. Also using to set maximum amount of ETH to be raised in crowdsale
+        uint open, // test: use fakenow
+        uint close // test: use fakenow + 1 minutes
+    
     )
         // Pass the constructor parameters to the crowdsale contracts.
-        NewCoinSale (rate,wallet,token)
+        Crowdsale (rate,wallet,token)
         CappedCrowdsale (goal)
-        TimedCrowdsale (fakenow, fakenow + 1 minutes) // test
-        // TimedCrowdsale (now, now + 24 weeks) // production
-        RefundablePostDeliveryCrowdsale (goal)
+        // TimedCrowdsale (fakenow, now + 1 minutes) // test
+        TimedCrowdsale (open, close) // production
+        RefundableCrowdsale (goal)
         public
     {
         // constructor can stay empty as logic included in contracts inherited from OpenZeppelin
@@ -38,24 +39,25 @@ contract NewCoinSaleDeployer {
 
     address public token_sale_address;
     address public token_address;
-
+    
     constructor(
         string memory name,
         string memory symbol,
         address payable wallet, // this address will receive all Ether raised by the sale
         uint goal
-        //uint fakenow // test
+        // uint fakenow // use for testing
+
     )
         public
     {
         // Create the NewCoin and keep its address handy
         NewCoin token = new NewCoin(name, symbol,0); // create new token w params name, symbol, initial supply
-        token_address = address(token); // fetchs token address
+        token_address = address(token); // fetch token address
         
         // Create the NewCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
-        NewCoinSale token_sale = new NewCoinSale(1, wallet, token, goal, cap, fakenow, fakenow + 1 minutes); // test
-        // NewCoinSale token_sale = new NewCoinSale(1, wallet, token, goal, cap, now, now + 24 weeks); // production
-        token_sale_address_address = address(token_sale);
+        //NewCoinSale token_sale = new NewCoinSale(1, wallet, token, goal, fakenow, now + 1 minutes); // test
+        NewCoinSale token_sale = new NewCoinSale(1, wallet, token, goal, now, now + 24 weeks); // production
+        token_sale_address = address(token_sale);
         
         // make the NewCoinSale contract a minter, then have the NewCoinSaleDeployer renounce its minter role
         token.addMinter(token_sale_address);
